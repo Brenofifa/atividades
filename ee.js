d70254ebdfdc66337859f630bfe3131206e5169f
Breno <brenopfiffer06@gmail.com>
2024-06-27 21:43:58 -0300

    feito de 1 ate 9

---
+++ ee.js
@@ -0,0 +1,84 @@
+let nome = 'joao'
+console.log(nome)
+
+
+// exercicio 2
+let a  =5, b = 10, soma
+soma = a + b
+console.log(soma)
+
+//exercicio 3 
+let primeironome = 'josefa', segundonome = 'silva'
+console.log(primeironome, segundonome)
+
+//exercicio 4 
+let pessoa  = {
+nome : 'Cleiton',
+idade : '18', 
+endereço : 'Ararangua',
+telefone : 992947634, 
+}
+
+//exercicio 5 
+let numeros = [1, 2, 3, 4, 5]
+console.log(numeros[0], numeros[4])
+
+
+//exercicio 6 
+let cores = ['vermelho', 'verde', 'azul' ]
+console.log(cores)
+
+
+// exercicio 7 
+let idade = 18
+if(idade < 18){
+console.log('menor')
+}else{
+    console.log('maior')
+}
+
+//exercicio 8 
+let nota = 8 
+if(nota >=7){
+    console.log('aprovada')
+}else{
+console.log ('recuperação')
+} 
+
+//exercicio 9 
+let diadasemana = 7
+switch (diadasemana){
+    case 1 : {
+        console.log('domingo')
+        break
+    }
+    
+     case 2 : {
+        console.log('segunda')
+        break
+    }
+    case 3 : {
+        console.log('terca')
+        break
+    }
+    case 4 : {
+        console.log('quarta')
+        break
+    }
+
+    case 5 : {
+        console.log('quinta')
+        break
+    }
+
+    case 6 : {
+        console.log('sexta')
+        break
+    }
+
+    case 7 : {
+        console.log('sabado')
+        break
+    }
+}
+
